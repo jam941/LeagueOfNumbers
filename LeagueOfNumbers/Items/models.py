791d3294d61838c7b3ed0e72d075c630ca573125
Jarred Moyer <jarredds941@gmail.com>
2022-02-05 01:00:09 -0500

    Stubbed Item model

--- LeagueOfNumbers/Items/models.py
+++ LeagueOfNumbers/Items/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    #TODO Verify database relationships for components, image, gold, tags, stats, and builds
+    id = models.IntegerField()
+    name = models.CharField()
+    colloq = models.CharField()
+    plaintext = models.CharField()
+    components = models.ManyToManyField()
+
+    #TODO Change this to reference ItemImage Model
+    images = models.CharField()
+    gold  = models.CharField()
+    tags = models.ManyToManyField()
+    stats = models.CharField()
+
+    depth = models.IntegerField()
+    builds = models.ManyToManyField()
